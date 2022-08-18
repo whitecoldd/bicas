762eb7379f51a6ff263c0c96ec4fd9ee1a6d853f
whitecoldd <buyukliwc@gmail.com>
2022-08-18 10:56:12 +0300

    make it better3

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("casino-front/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "casino-front", "build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || 9000, async () => {
   console.log("backend server is up on PORT " + process.env.PORT || 9000);
 });
